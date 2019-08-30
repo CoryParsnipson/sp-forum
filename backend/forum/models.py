a01888a223e141a84ce5da18d1b9769ac399362b
Cory Parsnipson <cparsnipson@gmail.com>
2019-08-30 16:39:41 -0700

    Add forum models and add Posts to index page.

--- backend/forum/models.py
+++ backend/forum/models.py
@@ -2,5 +2,33 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    """ Forum posts.
+    """
+
+    MAX_CHAR_LENGTH=50000
+    PREVIEW_CHAR_LENGTH=30
+
+    # TODO: on_delete=models.SET(<function to set author field to "deleted user">)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    content = models.TextField(max_length=MAX_CHAR_LENGTH)
+    published = models.DateTimeField(auto_now_add=True, verbose_name="date published")
+
+    def __str__(self):
+        model_text = "(" + (self.author.username if self.author else "Anonymous") + ") "
+        model_text += self.content if len(self.content) < self.PREVIEW_CHAR_LENGTH \
+            else (self.content[:self.PREVIEW_CHAR_LENGTH] + "...")
+        return model_text
+
+
+class Thread(models.Model):
+    pass
+
+
+class Forum(models.Model):
     pass
--- backend/forum/views.py
+++ backend/forum/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from .models import Post
+
 # Create your views here.
 def index(request):
-    return render(request, 'forum/index.html')
+    context = {}
+    context['posts'] = Post.objects.order_by('-published')[:40];
+    return render(request, 'forum/index.html', context)
 
 def post_detail(request):
     data = {
