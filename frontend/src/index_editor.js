67e436d71d68ed05c1d274aee7ebdd5f423ee65a
Cory Parsnipson <cparsnipson@gmail.com>
2019-09-26 22:22:06 -0700

    Install mini-css-extract-plugin and split webpack entrypoints into smaller pieces.
    When loading pages that don't have a div labeled for loading the React component,
    the browser throws a javascript error because the React code is unable to
    mount properly. The solution is to not load that code for pages that don't have
    any need for an editor React component mount.
    
    In order to achieve this, we need to change the django webpack loader config
    so that it doesn't load that javascript code unless we're on thread.html.
    Splitting the entrypoints into 'base' and 'editor' helps with this. Base
    contains only css so far (but may contain javascript too later on). Editor
    contains javascript and css for the React editor component.
    
    Now instead of loading the 'main' bundle (which was the default name), we
    load base for all pages and then additionally the editor bundle for the
    thread page.
    
    The django bundle loader also has a second parameter to filter bundle
    contents by file extension. This is kind of an unnecessary optimization
    at this point.

---
+++ frontend/src/index_editor.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import * as utils from './utils.js'
+
+import { Editor } from './editor/editor.js';
+
+window.props = {};
+window.props.editor_id = "editor1";
+window.props.editor_action = window.django.editor_action;
+window.props.post_meta = {
+   'thread': window.django.thread_id
+};
+window.props.editor_on_submit = function(event, editor) {
+   event.preventDefault();
+
+   // load up status bar input value with innerHTML of editor window
+   var post_contents = document.getElementById("editor-area").getElementsByClassName("editor-window-contents")[0];
+
+   var form_post_holder = document.forms.editor1_form.children.content;
+   form_post_holder.value = encodeURIComponent(post_contents.innerHTML);
+
+   var thread_id = encodeURIComponent(document.forms.editor1_form.children.thread.value);
+
+   // get response from url endpoint
+   fetch(window.props.editor_action, {
+      credentials: 'include',
+      method: 'post',
+      mode: 'same-origin',
+      headers: {
+         "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+         "X-CSRFToken": utils.getCookie('csrftoken')
+      },
+      body: 'content=' + form_post_holder.value + "&thread=" + thread_id
+   })
+   .then(function(response) {
+      response.json().then(function(data) {
+         var post_list = document.getElementsByClassName("posts")[0];
+         var num_rows = post_list.getElementsByTagName("tr").length;
+         var new_post = post_list.insertRow(num_rows - 1);
+
+         var post_info = document.createElement('th');
+         var post_content = new_post.insertCell(0);
+
+         post_content.insertAdjacentElement("beforebegin", post_info);
+
+         post_info.classList.add("post-meta");
+         post_content.classList.add("post-content");
+
+         post_info.innerHTML = data.author ? data.author.username : "Anonymous";
+         post_content.innerHTML = data.content;
+      })
+   })
+   .then(function(response) {
+      // remove selection ranges
+      document.getSelection().removeAllRanges();
+
+      // after successful post, clear editor contents
+      editor.window.current.clear();
+   });
+};
+
+ReactDOM.render(
+   <Editor
+      editor_id={window.props.editor_id}
+      action={window.props.editor_action}
+      post_meta={window.props.post_meta}
+      on_submit={window.props.editor_on_submit}
+   />,
+   document.getElementById('editor-area')
+);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,17 +1,24 @@
 var path = require('path');
 var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
    mode: "development",
    context: __dirname,
-   entry: './src/index.js',
+   entry: {
+      base: './src/index_base.js',
+      editor: './src/index_editor.js',
+   },
    output: {
       path: path.resolve('../backend/assets/webpack_bundles'),
       filename: "[name]-[hash].js",
    },
    plugins: [
-      new BundleTracker({filename: './webpack-stats.json'})
+      new BundleTracker({filename: './webpack-stats.json'}),
+      new MiniCssExtractPlugin({
+         filename: '[name]-[hash].css',
+      }),
    ],
    module: {
       rules: [
@@ -34,13 +41,34 @@ module.exports = {
             use: [
                'style-loader',
                'css-loader',
-            ],
+               {
+                  loader: MiniCssExtractPlugin.loader,
+                  options: {
+                     publicPath: (resourcePath, context) => {
+                        // publicPath is the relative path of the resource to the context
+                        // e.g. for ./css/admin/main.css the publicPath will be ../../
+                        // while for ./css/main.css the publicPath will be ../
+                        return path.relative(path.dirname(resourcePath), context) + '/';
+                     }
+                  }
+               }
+            ]
          },
          {
             test: /\.s[ac]ss$/i,
             exclude: /node_modules/,
             use: [
-               'style-loader',
+               {
+                  loader: MiniCssExtractPlugin.loader,
+                  options: {
+                     publicPath: (resourcePath, context) => {
+                        // publicPath is the relative path of the resource to the context
+                        // e.g. for ./css/admin/main.css the publicPath will be ../../
+                        // while for ./css/main.css the publicPath will be ../
+                        return path.relative(path.dirname(resourcePath), context) + '/';
+                     }
+                  }
+               },
                'css-loader',
                'sass-loader',
             ]
@@ -48,6 +76,6 @@ module.exports = {
       ]
    },
    resolve: {
-      extensions: ['*', '.js', '.jsx']
+      extensions: ['*', '.js', '.jsx', '.css', '.scss']
    }
 }
