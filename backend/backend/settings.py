0b56f50f4fe747b0a7d3e02cfccd432dfd7d9e04
Cory Parsnipson <cparsnipson@gmail.com>
2019-10-28 00:06:13 -0700

    Fix prod staticfile root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = "/assets" if os.environ["SERVER_MODE"] == "production" else os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
